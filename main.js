344c172c3e977f9d3e816519e64dc6de33a108b0
samuelcvega <samuelecv18@gmail.com>
2023-11-27 18:17:52 -0500

    seccion of form complete

--- main.js
+++ main.js
@@ -23,4 +23,22 @@ function setupCardRotation() {
 
 //Call main function
 setupCardRotation();
+const formulario = document.getElementById('formulario');
+        formulario.addEventListener('submit', (event) => {
+            event.preventDefault();
+            const nombre = document.getElementById('nombre').value;
+            const email = document.getElementById('email').value;
+            const pais = document.getElementById('pais').value;
+            const robot = document.getElementById('robot').checked;
+            const apellido = document.getElementById('apellido').value;
+            const empresa = document.getElementById('empresa').value;
+            const boletin = document.getElementById('boletin').checked;
+            
+            if (!robot) {
+                alert('Debes confirmar que no eres un robot');
+                return;
+            }
+            
+            alert(`Registro exitoso, ${nombre} ${apellido}! Tu país es ${pais}, tu empresa es ${empresa}, y ${boletin ? 'te has suscrito' : 'no te has suscrito'} al boletín.`);
+        });
 
