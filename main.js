6b379bb6c00a2eac507fa2ed242bc6d9a63fc1aa
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2023-12-10 20:42:08 -0500

    Added interactivity con card design elements and improve UI experience

--- main.js
+++ main.js
@@ -2,8 +2,27 @@ const d = document;
 const serviceCard = d.querySelector('.services__card');
 const btnDiscover = d.querySelector('#discoverBtn');
 const serviceCardBack = d.querySelector('.services__card--back');
+const titleContainer = d.querySelector('.services__card--back__title')
+const cardsBackItem = d.querySelectorAll('.services__card--back__item')
+const serviceCardsContainer = d.querySelector('.services__card--back__cards')
+const cardsHideBtn = d.querySelector('#hideCardsBtn')
+const btnDesign = d.querySelector('#design__btn')
+btnDesign.addEventListener('click', showHideDesign)
+serviceCardBack.addEventListener('mouseleave', showFront);
 
-function setupCardRotation() {
+btnDiscover.addEventListener('click', ()=> {
+    showBack();
+    backIn()
+});
+
+cardsHideBtn.addEventListener('click', ()=>{
+    showFront()
+    serviceCardsContainer.classList.add('hide')
+    titleContainer.classList.remove('hide')
+    cardsBackItem.forEach(item => item.style.animation = 'none')
+})
+
+
 
     function rotateCard(degrees) {
         serviceCard.style.transform = `rotateX(${degrees}deg)`;
@@ -17,12 +36,45 @@ function setupCardRotation() {
         rotateCard(180);
     }
 
-    btnDiscover.addEventListener('click', showBack);
+
+
     serviceCardBack.addEventListener('mouseleave', showFront);
+
+
+
+console.log(cardsBackItem)
+
+function backIn(){
+    titleContainer.style.opacity = '0';
+    titleContainer.style.animation = 'opacityTitleIn 2s 0.9s ease-in-out forwards';
+}
+
+
+
+function backOut(){
+    titleContainer.style.opacity = '1';
+    titleContainer.style.animation = 'opacityTitleOut 1.2s 0.4s ease-in-out forwards';
+} 
+
+function removeBackIn(){
+    titleContainer.classList.add('hide')
+    serviceCardsContainer.classList.remove('hide')
+    cardsBackItem.forEach((item, index) => {
+        const delay = `${index * 0.3}s`
+        item.style.opacity = '0';
+        item.style.animation = `opacityTitleIn 2s ${delay} ease-in-out forwards` ;
+        item.classList.remove('hide')
+    }) 
+}
+
+function showHideDesign(){
+    backOut()
+    setTimeout(removeBackIn, 1000)   
 }
 
 //Call main function
-setupCardRotation();
+
+
 const formulario = document.getElementById('formulario');
         formulario.addEventListener('submit', (event) => {
             event.preventDefault();
